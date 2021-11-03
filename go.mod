aefe1f5d20684a9f3411d3bd1b98845fa34aac80
Kaan Karakaya <yusufkaan142@gmail.com>
2021-11-04 01:41:26 +0300

    feat: test traefik proxy Signed-off-by: Kaan Karakaya <yusufkaan142@gmail.com>

--- go.mod
+++ go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.29.0 // indirect
--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"context"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
-	"github.com/docker/go-connections/nat"
 	"github.com/gofiber/fiber/v2"
+	"github.com/segmentio/ksuid"
 	"log"
 	"math/rand"
-	"strconv"
 	"time"
 
 	"github.com/docker/docker/client"
@@ -28,27 +27,38 @@ func main() {
 
 	app.Get("/give", func(c *fiber.Ctx) error {
 		//Generate safe port number from range 30000-32767
-		port := 30000 + rand.Intn(2767)
+		//port := 30000 + rand.Intn(2767)
+		//portStr := strconv.Itoa(port)
+
+		kid := ksuid.New().String()
+		//kid := "kabaca"
+
 		// Create a container
 		ctx := context.Background()
 		create, err := cli.ContainerCreate(ctx,
 			&container.Config{
 				Cmd:   []string{"dockerd", "--host=tcp://0.0.0.0:2375"},
 				Image: "docker:dind",
+				Labels: map[string]string{
+					"traefik.enable": "true",
+					"traefik.http.routers." + kid + ".rule":                      "Host(`" + kid + ".theykk.com`)",
+					"traefik.http.services." + kid + ".loadbalancer.server.port": "2375",
+				},
 			},
 			&container.HostConfig{
 				Privileged: true,
-				PortBindings: nat.PortMap{
-					"2375/tcp": {
-						{
-							HostPort: strconv.Itoa(port),
-						},
-					},
-				},
+				// Do not open port to public, use proxy instead
+				//PortBindings: nat.PortMap{
+				//	"2375/tcp": {
+				//		{
+				//			HostPort: portStr,
+				//		},
+				//	},
+				//},
 			},
 			nil,
 			nil,
-			"dockerverbana"+strconv.Itoa(port))
+			"dockerverbana_"+kid)
 		if err != nil {
 			log.Println(err)
 		}
@@ -57,7 +67,7 @@ func main() {
 		if err != nil {
 			log.Println(err)
 		}
-		return c.SendString(strconv.Itoa(port))
+		return c.SendString(kid)
 	})
 	log.Fatal(app.Listen(":8585"))
 }
